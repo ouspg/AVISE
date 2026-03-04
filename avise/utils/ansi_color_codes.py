8054274240a09d836d389ae36e0cf5e417741820
Zippo00 <mikko_lempinen@outlook.com>
2026-03-04 12:57:02 +0800

    added ansi_color util

---
+++ avise/utils/ansi_color_codes.py
@@ -0,0 +1,14 @@
+"""ANSI Color codes"""
+
+ansi_colors = {
+    "black": "\033[0;30m",
+    "red": "\033[0;31m",
+    "green": "\033[0;32m",
+    "yellow": "\033[0;33m",
+    "blue": "\033[0;34m",
+    "magenta": "\033[0;35m",
+    "cyan": "\033[0;36m",
+    "white": "\033[0;37m",
+    "bold_red": "\033[1;31m",
+    "reset": "\033[0m"  # Reset to default terminal color
+}
