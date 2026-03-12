92db28c91093bfcd8bbf917073b6c4afdd39b773
Zippo00 <mikko_lempinen@outlook.com>
2026-03-12 21:51:44 +0800

    alm & elm AcceleratorError messages typos fixed

--- avise/models/evaluation_lm.py
+++ avise/models/evaluation_lm.py
@@ -76,12 +76,12 @@ class EvaluationLanguageModel:
                 )
             except AcceleratorError as e:
                 logger.error(
-                    f"Ran into an issue while loading model to GPU. If you're using an older GPU, try installing an older version of torch (e.g. pip install torch==2.7.1). Alternatively, you can load the model into CPU by setting the value of 'evaluation_model_device' field to 'cpu' in the SET configuration file..\n{e}"
+                    f"Ran into an issue while loading model to GPU. If you're using an older GPU, try installing an older version of torch (e.g. pip install torch==2.7.1). Alternatively, you can load the model into CPU by setting the value of 'evaluation_model_device' field to 'cpu' in the SET configuration file.\n{e}"
                 )
             sys.exit(1)
         except AcceleratorError as e:
             logger.error(
-                f"Ran into an issue while loading model to GPU. If you're using an older GPU, try installing an older version of torch (e.g. pip install torch==2.7.1). Alternatively, you can load the model into CPU by setting the value of 'evaluation_model_device' field to 'cpu' in the SET configuration file..\n{e}"
+                f"Ran into an issue while loading model to GPU. If you're using an older GPU, try installing an older version of torch (e.g. pip install torch==2.7.1). Alternatively, you can load the model into CPU by setting the value of 'evaluation_model_device' field to 'cpu' in the SET configuration file.\n{e}"
             )
             sys.exit(1)
         self.conversation_history = conversation_history
