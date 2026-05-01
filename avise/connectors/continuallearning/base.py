fab1de47f7dbd81ce557788d2451930eb14fdeba
Zippo00 <mikko_lempinen@outlook.com>
2026-05-01 16:28:41 +0800

    created base class for CL connectors & GenericRESTCLConnector is WiP

--- avise/connectors/continuallearning/base.py
+++ avise/connectors/continuallearning/base.py
@@ -1,4 +1,7 @@
-"""TODO"""
+"""Base class for CL Connectors.
+
+A Connector acts as the bridge between Security Evaluation Tests and a target system.
+"""
 
 import logging
 from abc import ABC, abstractmethod
@@ -8,14 +11,32 @@ logger = logging.getLogger(__name__)
 
 
 class BaseCLConnector(ABC):
-    """A connector handles communication with a specific API / backend,
-    abstracting the API usage for the framework.
-    This allows SET cases to be written only once and users are able to run them against different models with different configurations.
+    """A connector handles communication with a specific API / backend, abstracting
+    the API usage for the framework. This allows SET cases to be written only once
+    and users are able to run them against different models with different configurations.
 
     Class Methods:
+        - query: Query the target model.
 
     Class Attributes:
-
+        - config: Connector configuration data.
     """
 
-    pass
+    config: dict = {}
+
+    @abstractmethod
+    def query(self, data: list) -> dict:
+        """"""
+        pass
+
+    @abstractmethod
+    def status_check(self) -> bool:
+        """Perform a status check for the target API via a GET request.
+
+        Returns:
+            True if status check was successful.
+
+        Raises:
+            Exception: If the target API is not reachable.
+        """
+        pass
--- avise/connectors/continuallearning/generic.py
+++ avise/connectors/continuallearning/generic.py
@@ -1,4 +1,4 @@
-"""TODO"""
+"""Generic REST API Connector for Continual Learning systems."""
 
 import logging
 import requests
@@ -10,20 +10,145 @@ from ...utils import ansi_colors
 logger = logging.getLogger(__name__)
 
 
-@connector_registry.register("generic-rest-lm")
+@connector_registry.register("generic-rest-cl")
 class GenericRESTCLConnector(BaseCLConnector):
-    """Connector for communicating with custom REST APIs.
+    """Connector for communicating with custom Continual Learning REST APIs.
 
-    Used by Continual Learning SETs for ..
+    Used by Continual Learning SETs for querying a target model.
     """
 
     name = "generic-rest-cl"
 
-    def __init__(self, config: dict, evaluation: bool = False):
+    def __init__(self, config: dict):
         """Initialize the Generic REST API connector.
 
         Args:
             config: Dictionary containing data from Connector configuration JSON.
-            evaluation: Boolean flag indicating whether initializing a connector for the target model or the evaluation model
         """
         self.config = config
+        try:
+            self.base_url = config["target_model"]["api_endpoint"]["base"].get(
+                "url", ""
+            )
+            if not self.base_url:
+                raise ValueError(
+                    '[["target_model"]"api_endpoint"]["base"]["url"] is not configured in the provided Generic REST CL Connector configuration file.'
+                )
+            if not isinstance(self.base_url, str):
+                raise TypeError(
+                    '[["target_model"]"api_endpoint"]["base"]["url"] must be a string in the Generic REST CL Connector configuration file.'
+                )
+            self.base_headers = config["target_model"]["api_endpoint"]["base"].get(
+                "headers", ""
+            )
+            self.base_api_key = config["target_model"]["api_endpoint"]["base"].get(
+                "api_key", ""
+            )
+            if not isinstance(self.base_api_key, (str, type(None))):
+                raise TypeError(
+                    '[["target_model"]"api_endpoint"]["base"]["api_key"] must be a string or null in the Generic REST CL Connector configuration file.'
+                )
+            self.base_method = config["target_model"]["api_endpoint"]["base"].get(
+                "method", "POST"
+            )
+            if not self.base_method:
+                self.base_method = "POST"
+            if not isinstance(self.base_api_key, str):
+                raise TypeError(
+                    '[["target_model"]"api_endpoint"]["base"]["method"] must be a string in the Generic REST CL Connector configuration file.'
+                )
+            self.base_pred_field = config["target_model"]["api_endpoint"]["base"].get(
+                "prediction_field", ""
+            )
+            if not isinstance(self.base_pred_field, str):
+                self.base_pred_field = str(self.base_pred_field)
+            if not self.base_pred_field:
+                raise ValueError(
+                    '["target_model"]["api_endpoint"]["base"]["prediction_field"] is required in the Generic REST CL Connector configuration file. It is the API response field that contains the target model´s prediction or response.'
+                )
+            if "train" in config["target_model"]["api_endpoint"]:
+                if (
+                    config["target_model"]["api_endpoint"]["train"].get("url")
+                    is not None
+                ):
+                    self.train_url = config["target_model"]["api_endpoint"][
+                        "train"
+                    ].get("url")
+                    if not isinstance(self.train_url, str):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["train"]["url"] must be a string in the Generic REST CL Connector configuration file.'
+                        )
+                    self.train_headers = config["target_model"]["api_endpoint"][
+                        "train"
+                    ].get("headers")
+                    self.train_api_key = config["target_model"]["api_endpoint"][
+                        "train"
+                    ].get("api_key")
+                    if not isinstance(self.train_api_key, (str, type(None))):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["train"]["api_key"] must be a string or null in the Generic REST CL Connector configuration file.'
+                        )
+                    self.train_method = config["target_model"]["api_endpoint"][
+                        "train"
+                    ].get("method", "POST")
+                    if not isinstance(self.train_method, str):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["train"]["method"] must be a string in the Generic REST CL Connector configuration file.'
+                        )
+            if "inference" in config["target_model"]["api_endpoint"]:
+                if (
+                    config["target_model"]["api_endpoint"]["inference"].get("url")
+                    is not None
+                ):
+                    self.infer_url = config["target_model"]["api_endpoint"][
+                        "inference"
+                    ].get("url")
+                    if not isinstance(self.infer_url, str):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["inference"]["url"] must be a string in the Generic REST CL Connector configuration file.'
+                        )
+                    self.infer_headers = config["target_model"]["api_endpoint"][
+                        "inference"
+                    ].get("headers")
+                    self.infer_api_key = config["target_model"]["api_endpoint"][
+                        "inference"
+                    ].get("api_key")
+                    if not isinstance(self.infer_api_key, (str, type(None))):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["inference"]["api_key"] must be a string or null in the Generic REST CL Connector configuration file.'
+                        )
+                    self.infer_method = config["target_model"]["api_endpoint"][
+                        "inference"
+                    ].get("method", "POST")
+                    if not isinstance(self.infer_method, str):
+                        raise TypeError(
+                            '["target_model"]["api_endpoint"]["inference"]["method"] must be a string in the Generic REST CL Connector configuration file.'
+                        )
+        except (KeyError, ValueError) as e:
+            logger.error(
+                f"{ansi_colors['red']}ERROR while generating initializing GenericRESTCLConnector: {e}{ansi_colors['reset']}"
+            )
+        logger.info(
+            f"  Generic REST API Connector Initialized for Continual Learning target system."
+        )
+        logger.info(f"  Base URL: {self.base_url}")
+        if self.base_api_key is not None:
+            logger.info(
+                f"  Base API Key: {'*' * 8}...{self.base_api_key[-4:] if len(self.base_api_key) > 4 else '****'}"
+            )
+        if self.train_api_key is not None:
+            logger.info(
+                f"  Train API Key: {'*' * 8}...{self.train_api_key[-4:] if len(self.train_api_key) > 4 else '****'}"
+            )
+        if self.infer_api_key is not None:
+            logger.info(
+                f"  Inference API Key: {'*' * 8}...{self.infer_api_key[-4:] if len(self.infer_api_key) > 4 else '****'}"
+            )
+
+    def query(self, data: list):
+        """TODO"""
+        pass
+
+    def status_check(self):
+        """TODO"""
+        pass
